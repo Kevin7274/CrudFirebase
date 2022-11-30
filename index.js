5a513c41ab51fc0fa4effe568356dac456a06820
Kevin7274 <kuhes0918@gmail.com>
2022-11-29 21:01:14 -0600

    configurando el indexJS

---
+++ index.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
+import { } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+apiKey: "AIzaSyBfVAZSyfmN6PkyWVBiahG3X7gwNEbKfYg",
+authDomain: "thecrud-6ff76.firebaseapp.com",
+projectId: "thecrud-6ff76",
+storageBucket: "thecrud-6ff76.appspot.com",
+messagingSenderId: "915641082520",
+appId: "1:915641082520:web:8ea15895dc24dbce4a26ed"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
